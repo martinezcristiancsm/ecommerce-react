e60e1b7bd865be656e3aaa89e703a4ec371077d8
Cristian Martinez <marinezcristiancsm@gmail.com>
2023-11-13 23:40:54 -0300

    aver

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -15,7 +15,7 @@ const SingleProduct = () => {
     const {id} = useParams();
     
     useEffect(() => {
-      fetch("/src/products.json").then(res => res.json()).then(data => setProducts(data))
+      fetch("./../src/products.json").then(res => res.json()).then(data => setProducts(data))
     }, [])
 
     const result = product.filter((p) => p.id === id);
